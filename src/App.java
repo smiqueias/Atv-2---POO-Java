3a956d9e60f6d10ea03971fe56ed3ca7b9d07fc1
Saulo <saulomiqueias7@gmail.com>
2021-09-10 22:30:26 -0300

    done

--- src/App.java
+++ src/App.java
@@ -3,11 +3,11 @@ public class App {
 
         Aluno aluno1 = new Aluno("Saulo","1111111","Rua x","20201021");
         Aluno aluno2 = new Aluno( "Miqueias","222222222","Rua y","20201022" );
-        Aluno aluno3 = new Aluno( "Nascimento","33333333333","Rua z","20201024");
+        Aluno aluno3 = new Aluno( "Nascimento","33333333333","Rua z","20201022");
 
 
         Coordenador coordenador = new Coordenador("Joao","071123123121",
-                "Rua x","14",6500);
+                "Rua x","14",6500,"");
 
         Professor professor = new Professor("Getulio","12112134512","Rua xc",
                 "12",3500,"TI");
--- src/Coordenador.java
+++ src/Coordenador.java
@@ -2,8 +2,9 @@ public class Coordenador extends Funcionario{
 
     private String course;
 
-    public Coordenador(String name, String cpf, String address, String code, double salary) {
+    public Coordenador(String name, String cpf, String address, String code, double salary, String course) {
         super(name, cpf, address, code, salary);
+        this.course = course;
     }
 
     public String getCourse() {
